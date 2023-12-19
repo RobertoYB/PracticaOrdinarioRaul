81f7f144ed712cc6783c4f05d502b92e7df3afba
Emilio Campos Daguer <emcam02pa@gmail.com>
2023-12-18 19:32:45 -0600

    ICuenta, IElementoCuenta y clases
    En este commit se hacen los cambios correspondientes a las Interfaces de ICuenta y IElementoCuenta así como sus implementaciones en sus respectivas clases

---
+++ PracticaOrdinarioRaul/ICuenta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public interface ICuenta
+{
+    private List<ElementoCuenta> _elementosCuenta;
+
+    public void AgregarElemento(Platillo platillo, Cantidad cantidad);
+
+    public void MostrarCuenta();
+}
