61fed1325cc8b5e041a2cfc838d297cef1d30f0c
RobertoYB <146978884+RobertoYB@users.noreply.github.com>
2023-12-18 18:18:57 -0600

    Subida de datos básicos de visual studio
    Agregue lo necesario para empezar el trabajo

---
+++ PracticaOrdinarioRaul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PracticaOrdinarioRaul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
